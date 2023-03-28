88fb27da01b331e1bb1eb25e1dde6a359eed72b9
Akshat Shah <aksshah2004@gmail.com>
2023-03-27 20:53:42 -0500

    start Login page

--- Login.js
+++ Login.js
@@ -1,22 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 export function Login() {
- return (
+  return (
     <View style={styles.container}>
-      <Text>Login Page </Text>
+      <Text>Login Page</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
-
-
-
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
