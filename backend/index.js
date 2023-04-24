9016c420753ca7902be03ac10216142fa88d38f4
Greg Zachariah <gregoryzach@gmail.com>
2023-04-23 19:20:09 -0500

    Changes

---
+++ backend/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = 50005;
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, () => {console.log(`Server is running on port ${PORT}`)});
